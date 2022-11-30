1a3f23c5d4b1c91f78780706e95425f2e7a1c9b6
GideoN <31788167+GideoNguyen@users.noreply.github.com>
2022-11-30 23:34:02 +0700

    Add files via upload

--- Source/pages/SanPhamTheoLoai/pIndex.php
+++ Source/pages/SanPhamTheoLoai/pIndex.php
@@ -1,1 +1,43 @@
+<div class="container">
+    <?php 
+        if (isset($_GET["sub"]))
+            $sub = $_GET["sub"];
+        else{
+            DataProvider::ChangeURL("index.php?a=404");
+        }
+        $sql = "SELECT tenchitietloaisanpham FROM chitietloaisanpham WHERE BiXoa = 0 AND MaChiTietLoaiSanPham = ".$sub;
+        $result = DataProvider::ExecuteQuery($sql);
+        while($row = mysqli_fetch_array($result))
+        {
+            ?>
+                <div class="title">
+                <a href="#" class="h2"><?php echo $row["tenchitietloaisanpham"];?></a>
+                </div>
+            <?php
+        }
+    ?>
+    <div class="row">
+	<?php 
+		$sql = "SELECT * FROM sanpham WHERE BiXoa = 0 AND MaChiTietLoaiSanPham = ".$sub;
+		$result = DataProvider::ExecuteQuery($sql);
+		while($row = mysqli_fetch_array($result))
+		{
+			?>
+			<div class="col">
+				<div class="card product-item">
+					<img class="card-img-top" src="images/<?php echo $row["HinhURL"]?>" alt="<?php echo $row["MoTa"];?>">
+					<div class="card-body">
+						<h4 class="card-title"><?php echo $row["TenSanPham"];?></h4>
+						<p class="card-text"><b>Giá: <span class="text-danger"><?php echo $row["GiaSanPham"];?>đ</span></b></p>
+						<a href="index.php?a=8&id=<?php echo $row["MaSanPham"];?>" class="btn btn-primary">CHI TIẾT</a>
+					</div>
+				</div>
+			</div>
+			<?php
+		}
+	?>
+	</div>
+</div>
+
+
 
